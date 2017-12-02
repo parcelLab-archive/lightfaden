a30c19eea4105405fd12c4dea5b3517fbfed4475
Dennis Schröder <dennis@wearsatie.com>
2017-12-02 20:53:43 +0100

    Screw you DynamoDB

--- server.js
+++ server.js
@@ -6,67 +6,108 @@ const products = require('./lib/products');
 const ineedhelp = require('./lib/ineedhelp');
 const showmemybill = require('./lib/showmemybill');
 const lightfaden = require('./lib/lightfaden');
+const mongoose = require('mongoose');
 
-const app = express();
-app.use(logger('dev'));
+const MONGOPATH = 'mongodb://localhost/lighfaden';
+const MONGOOPTIONS = {
+  useMongoClient: true,
+  native_parser: true,
+  poolSize: 5,
+  keepAlive: true,
+  socketOptions: {
+    socketTimeoutMS: 0,
+    connectionTimeout: 0,
+  },
+};
 
-app.use((req, res, next) => {
-  if (req.query.userId) {
-    utils.createUser(req.query.userId, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({msg: err});
-      }
-      else {
-        console.log(data);
-        next();
-      }
-    });
-  } else return res.status(500).json({msg: 'missing userId'});
+mongoose.connection.on('connected', function () {
+  console.log('> Connected to MongoDB...');
+
+
+  const app = express();
+  app.use(logger('dev'));
+
+  app.use((req, res, next) => {
+    if (req.query.userId) {
+      utils.createUser(req.query.userId, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({msg: err});
+        }
+        else {
+          console.log(data);
+          next();
+        }
+      });
+    } else return res.status(500).json({msg: 'missing userId'});
+  });
+
+  app.get('/activity', function(req, res) {
+    var q = req.query;
+    if (q.activity && q.target) {
+      activity.setActivity(q.activity, q.target, (err, msg) => {
+        if (err) res.status(500).json({msg: err});
+        else res.status(200).json({msg: msg});
+      });
+    } else {
+      res.json({msg: 'missing paramater (activity, target)'});
+    }
+  });
+
+  app.get('/showmemybill', function(req, res) {
+    res.status(501).send();
+  });
+
+  app.get('/ineedhelp', function(req, res) {
+    res.status(501).send();
+  });
+
+  app.get('/products', function(req, res) {
+    var q = req.query;
+    if (q.name && q.price && q.peruse) {
+      products.addProduct(q.userId, q.name, q.price, q.peruse, (err, data) => {
+        if (err) res.status(500).json({msg: err});
+        else res.status(200).json({msg: data});
+      });
+    } else {
+      res.status(500).json({msg: 'missing paramater (name, price, peruse)'});
+    }
+  });
+
+  app.get('/lightfaden', function(req, res) {
+    res.status(501).send();
+  });
+
+  var server = app.listen(8080, function(err) {
+    if (err) console.log(err);
+    else console.log('server is running: http://localhost:3000');
+  });
 });
 
-app.get('/activity', function(req, res) {
-  var q = req.query;
-  if (q.activity && q.target) {
-    activity.setActivity(q.activity, q.target, (err, msg) => {
-      if (err) res.status(500).json({msg: err});
-      else res.status(200).json({msg: msg});
-    });
-  } else {
-    res.json({msg: 'missing paramater (activity, target)'});
-  }
+/////////////////////////////////
+// MongoDB Connection Handling //
+/////////////////////////////////
+
+mongoose.connection.on('error', function (err) {
+  console.error('> Failed to connect to MongoDB on startup ', err);
 });
 
-app.get('/showmemybill', function(req, res) {
-  res.status(501).send();
+mongoose.connection.on('disconnected', function () {
+  console.log('> Mongoose default connection to MongoDB disconnected');
 });
 
-app.get('/ineedhelp', function(req, res) {
-  res.status(501).send();
-});
+var gracefulExit = function () {
+  mongoose.connection.close(function () {
+    console.log('> MongoDB disconnected through app termination');
+    process.exit(0);
+  });
+};
 
-app.get('/products', function(req, res) {
-  var q = req.query;
-  if (q.name && q.price && q.peruse) {
-    products.addProduct(q.userId, q.name, q.price, q.peruse, (err, data) => {
-      if (err) res.status(500).json({msg: err});
-      else res.status(200).json({msg: data});
-    });
-  } else {
-    res.status(500).json({msg: 'missing paramater (name, price, peruse)'});
-  }
-});
+process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
 
-app.get('/lightfaden', function(req, res) {
-  res.status(501).send();
-});
-
-var server = app.listen(8080, function(err) {
-  if (err) console.log(err);
-  else console.log('server is running: http://localhost:3000');
-});
-
-process.on('SIGINT', function(){
-  console.log('byebye');
-  server.close();
-});
+try {
+  mongoose.connect(MONGOPATH, MONGOOPTIONS);
+  console.log('> Trying to connect to MongoDB...');
+} catch (err) {
+  console.log('> Sever initialization failed ', err.message);
+}
