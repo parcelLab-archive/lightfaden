3f798c410a4b116478a8c8c24d48bafac3e94f1e
Julian Krenge <julian@parcellab.com>
2017-12-03 09:05:42 +0100

    Finalized dashboard

--- ui/views/dashboard.js
+++ ui/views/dashboard.js
@@ -3,7 +3,20 @@
 module.exports = (numOfUsers, numOfGuides) => `
 <div class="row">
   <div class="col-md-4">
-    <div class="well">
+    <div class="well" style="height: 150px;">
+      <b>Global settings</b>
+      <hr />
+      <form class="form-inline">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox"> Required authenticated users
+          </label>
+        </div>
+      </form>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="well" style="height: 150px;">
       <center>
         <h1>${numOfUsers}</h1>
         users
@@ -11,7 +24,7 @@ module.exports = (numOfUsers, numOfGuides) => `
     </div>
   </div>
   <div class="col-md-4">
-    <div class="well">
+    <div class="well" style="height: 150px;">
       <center>
         <h1>${numOfGuides}</h1>
         guides
